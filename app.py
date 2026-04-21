8a538de0efb50322636d979fee5d5d55e7ac6c25
waelbouzoubaa <waelbouzoubaa@gmail.com>
2026-04-21 10:20:04 +0200

    logo rond en barre de fenetre

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ load_dotenv()
 
 st.set_page_config(
     page_title="Meet Notes — Ramery",
-    page_icon="assets/logo.png",
+    page_icon="assets/favicon_round.png",
     layout="wide",
     initial_sidebar_state="expanded",
 )
