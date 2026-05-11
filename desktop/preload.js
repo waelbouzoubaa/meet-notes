c8b2e6d1b905bd1188a3ec41bf192fcd7707f405
waelbouzoubaa <waelbouzoubaa@gmail.com>
2026-05-11 10:50:46 +0200

    chore: ignore desktop folder — kept local only

--- desktop/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('kaptnotes', {
-  getPrimarySource: () => ipcRenderer.invoke('get-primary-source'),
-  getApiUrl:        () => ipcRenderer.invoke('get-api-url'),
-});
